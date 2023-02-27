daadad59abbba033eea20afe9003dbdf7998e248
sajidhanif555 <sajidhanif555@gmail.com>
2023-02-27 12:48:38 +0500

    React App Proj uploaded

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+const App = () => {
+    return (<>
+        <h1>Hello React</h1>
+        <h1>Sajid Hanif</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod optio eius eveniet reprehenderit. Delectus soluta laboriosam eveniet neque, ex repellendus nemo vel possimus voluptas, quidem nulla rem quis suscipit repudiandae quas beatae quo. In ab earum quia veniam cum tempora, aperiam quod ad quos molestias aliquid necessitatibus qui repudiandae dignissimos?</p>
+    </>);
+};
+export default App;
